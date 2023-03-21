86a14045a0528e31ace0b00288fc4893f103a996
MikePio <michelepiopilla@gmail.com>
2023-03-21 23:40:49 +0100

    Added EventListener to generate the standard price

--- README.md
+++ README.md
@@ -8,17 +8,19 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
 
-##Steps
+## Steps
 
--
--
--
--
--
--
--
--
--
+- salvo in una variabile i dati del input che chiede Num. di kilometri da percorrere in treno
+- salvo in una variabile i dati del input che chiede il nome e il cognome del passeggero
+- salvo in una variabile i dati dell'option nella select che chiede se il passeggero è maggiornenne o minorenne o over65
+- salvo in una varibile il prezzo del biglietto standard che è definito in base ai km (0.21 € al km) 
+- creo una condizione if dove salvo in una varibile uno sconto del 20% per i minorenni
+- creo una condizione else if dove salvo in una varibile uno sconto del 40% per gli over 65
+- creo una condizione else dove salvo in una varibile il prezzo del biglietto standard per adulti che è definito in base ai km (0.21 € al km)
+- creo una variabile con la Math random per generare un numero casuale random da 1 a 5 per le carrozze 
+- creo una variabile con la Math random per generare un numero casuale random da 1 a 99999 per il codice CP 
+- creo un EventListener per generare l'output cioè mostrare i dati inseriti negli input nel momento in cui viene cliccato il pulsante "Genera"
+- creo una variabile booleana ed un EventListener per nascondere e mostrare i dati del biglietto nel momento in cui viene cliccato il pulsante "Genera"
 
 
 
--- script.js
+++ script.js
@@ -9,12 +9,44 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 */
 
 
+// - salvo in una variabile i dati del input che chiede Num. di kilometri da percorrere in treno
+// - salvo in una variabile i dati del input che chiede il nome e il cognome del passeggero
+// - salvo in una variabile i dati dell'option nella select che chiede se il passeggero è maggiornenne o minorenne o over65
+// - salvo in una varibile il prezzo del biglietto standard che è definito in base ai km (0.21 € al km) 
+// - creo una condizione if dove salvo in una varibile uno sconto del 20% per i minorenni
+// - creo una condizione else if dove salvo in una varibile uno sconto del 40% per gli over 65
+// - creo una condizione else dove salvo in una varibile il prezzo del biglietto standard per adulti che è definito in base ai km (0.21 € al km)
+// - creo una variabile con la Math random per generare un numero casuale random da 1 a 5 per le carrozze 
+// - creo una variabile con la Math random per generare un numero casuale random da 1 a 99999 per il codice CP 
+// - creo un EventListener per generare l'output cioè mostrare i dati inseriti negli input nel momento in cui viene cliccato il pulsante "Genera"
+// - creo una variabile booleana ed un EventListener per nascondere e mostrare i dati del biglietto nel momento in cui viene cliccato il pulsante "Genera"
 
 
 
+const nameInput = document.getElementById('name-input');
+const kmInputs = document.getElementById('km-inputs');
+const ageSelect = document.getElementById('age-select');
+const ageValue = ageSelect.value
+const generateBtn = document.querySelector('.generate-btn');
+const resetBtn = document.querySelector('.reset-btn');
 
+let userPrice;
 
+console.log(nameInput);
+console.log(kmInputs);
+console.log(ageSelect);
+console.log(ageValue);
+console.log(generateBtn);
+console.log(resetBtn);
 
+// prezzo (0.21 * i km) generato per ogni volta che si clicca il pulsante generateBtn
+generateBtn.addEventListener('click', function(){
+  const kmValue = parseFloat(kmInputs.value);
+  userPrice = .21 * kmValue;
+  console.log(userPrice);
+  console.log(`Il prezzo per ${kmValue}km è ${userPrice}€.`);
+
+});
 
 
 
